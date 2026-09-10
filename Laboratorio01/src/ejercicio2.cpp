0bc87f23241ce3e4b420117fb58d046392b8e132
luqueGG <fluqueg@unsa.edu.pe>
2026-09-10 11:58:19 -0500

    [Laboratorio 01] - Left manipulator agregado

--- Laboratorio01/src/ejercicio2.cpp
+++ Laboratorio01/src/ejercicio2.cpp
@@ -7,21 +7,22 @@
 
 void ejercicio2()
 {
-	std::cout << "\nEjercicio 2\n";
+    std::cout << "\nEjercicio 2\n";
 
-	std::cout << std::setw(10) << "Nombre"
-			  << std::setw(8) << "Edad"
-			  << std::setw(12) << "Puntos"
-			  << "\n";
-	
-	std::cout << std::setw(10) << "S. Ohtani"
-			  << std::setw(8) << 32
-			  << std::setw(12) << "1.045/9.8"
-			  << "\n";
-	
-	std::cout << std::setw(10) << "A. Judge"
-			  << std::setw(8) << 34
-			  << std::setw(12) << "1.145/10.1"
-			  << "\n";
+    std::cout << std::left
+              << std::setw(15) << "Nombre"
+              << std::setw(8)  << "Edad"
+              << std::setw(12) << "Puntos"
+              << '\n';
+
+    std::cout << std::setw(15) << "S. Ohtani"
+              << std::setw(8)  << 32
+              << std::setw(12) << "1.045/9.8"
+              << '\n';
+
+    std::cout << std::setw(15) << "A. Judge"
+              << std::setw(8)  << 34
+              << std::setw(12) << "1.145/10.1"
+              << '\n';
 }
 
